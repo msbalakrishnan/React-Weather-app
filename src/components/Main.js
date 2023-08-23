928df8ee47b94ca6bc54e4819b8de53c40ac8c12
bala44858 <bala44858@gmail.com>
2023-08-23 16:30:18 +0530

    data fetched and displayed successfully ..

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,16 @@ import { useState } from "react";
 
 const Main = ()=>{
     const [city,setcity] = useState("");
-    const [ alldatails, setdatails] = useState({});
-    
+    const [ alldatails, setdatails] = useState({cod:404});
+    const [error , seterror]= useState(false);
+    const call = ()=>{
+        // alert("sdfn");
+        console.log("the state value is ");
+        console.log(alldatails);
+    }
     const onchanges = (e)=>{
         setcity(e.target.value);
-        console.log(city);
+        // console.log(city);
     }
     const search = ()=>{
         // var c = city;
@@ -27,9 +32,17 @@ const Main = ()=>{
             (data)=>{
                 setdatails(data);
                 console.log(data);
-                console.log(alldatails);
+                // console.log(dar)
+                // console.log(data.cod);
+                // console.log(alldatails);
 
             }
+        ).then(
+            (data)=>{
+                console.log(alldatails);
+            }
+        ).catch(
+            (e)=> seterror(e)
         )
 
 
@@ -37,10 +50,45 @@ const Main = ()=>{
     }
     return (
         <div>
+            <div className="error">
+
+                {error&& error}
+
+            </div>
+
             <form>
                 <input type="text" value={city} onChange={(e)=>{ onchanges(e) }}></input>
                 <input type="button" value=" search " onClick={search}></input>
+            {/* <button onClick={()=> call()} > check</button> */}
             </form>
+            <div className="data">
+                <div>
+                    latitude : {alldatails.cod!=404 && alldatails.coord.lat}
+                </div>
+                <div>
+                    longitude : {alldatails.cod!=404 && alldatails.coord.lon}
+                </div>
+                <div>
+                    city : {alldatails.cod!=404 && alldatails.name}
+                </div>
+                <div>
+                    Wind speed : {alldatails.cod!=404 && alldatails.wind.speed}
+                </div>
+                <div>
+                    tempterature : {alldatails.cod!=404 && alldatails.main.temp}
+                </div>
+                <div>
+                    max temp: {alldatails.cod!=404 && alldatails.main.temp_max}
+                </div>
+                <div>
+                    min temp : {alldatails.cod!=404 && alldatails.main.temp_min}
+                </div>
+
+                <div>
+                    humidity : {alldatails.cod!=404 && alldatails.main.humidity}
+                </div>
+               {/* {alldatails.cod!=404 && "bala ms"} */}
+            </div>
 
         </ div >
     )
